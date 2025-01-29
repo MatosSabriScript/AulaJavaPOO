d540ff66d24b06e83ecc4d62c5cc506499c35f37
Sabrina Matos Almeida <sabrinamatosalmeida2@gmail.com>
2025-01-29 08:35:26 -0300

    aula do dia 28

--- src/main/java/com/senai/exemplo/pessoa/Main.java
+++ src/main/java/com/senai/exemplo/pessoa/Main.java
@@ -1,4 +1,11 @@
 package com.senai.exemplo.pessoa;
 
 public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1= new Pessoa("Nicoly", 20);
+
+
+        System.out.println(pessoa1.altura + ", " + pessoa1.idade + ", " + pessoa1.nome +
+                ", " + pessoa1.endereco);
+    }
 }
--- src/main/java/com/senai/exemplo/pessoa/Pessoa.java
+++ src/main/java/com/senai/exemplo/pessoa/Pessoa.java
@@ -1,4 +1,27 @@
 package com.senai.exemplo.pessoa;
 
 public class Pessoa {
+    String nome;
+    int idade;
+    float altura;
+    String endereco;
+
+    public Pessoa(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+
+    }
+
+    public void falar(String frase){
+        System.out.println("A pessoa falou: " + frase);
+    }
+
+    public void comer(String alimento){
+        System.out.println("A pessoa comeu: " + alimento);
+    }
+
+    @Override
+    public String toString() {
+        return "O nome da pessoa é "+this.nome+"tem "+this.idade+ "e tem de altura. "+this.altura+ "Seu endereço é "+ this.endereco;
+    }
 }
